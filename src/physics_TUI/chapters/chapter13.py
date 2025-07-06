cf51f8c70c6bc6a1fd60b1452b6209931df9f369
ClaudioRMalvino <claudiormal@gmail.com>
2025-07-05 22:26:22 -0500

    Added some more functions to chapter13.py. Will finish the remaining functions by Monday.

--- src/physics_TUI/chapters/chapter13.py
+++ src/physics_TUI/chapters/chapter13.py
@@ -1,5 +1,6 @@
 from typing import List, Optional, Dict
 from physics_TUI.base_chapter import PhysicsChapter, Equation, Definition
+from math import sqrt, cos, acos
 
 # Global constants
 G: float = 6.674E-11 # Newton's gravitational constant
@@ -34,7 +35,7 @@ class Chapter13(PhysicsChapter):
                 formula="g = GM/r²",
                 variables={
                     "g": "Acceleration due to gravity (m/s²)",
-                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² [constant]",
+                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² (constant)",
                     "M": "Mass of the stellar body (kg)",
                     "r": "Radius of the stellar body (m)",
                 },
@@ -45,7 +46,7 @@ class Chapter13(PhysicsChapter):
                 formula="U = -GMm/r",
                 variables={
                     "U": "Gravitational potential energy (J)",
-                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² [constant]",
+                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² (constant)",
                     "M": "Mass of the stellar body (kg)",
                     "m": "Mass of the object (kg)",
                     "r": "Distance from the center of mass of M to  m (m)"
@@ -56,7 +57,7 @@ class Chapter13(PhysicsChapter):
                 name="Conservation of energy",
                 formula="½mv(1)² - GMm/r(1) = ½mv(2)² - GMm/r(2)",
                 variables={
-                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² [constant]",
+                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² (constant)",
                     "m": "Mass of the object (kg)",
                     "M": "Mass of the stellar body (kg)",
                     "v(1)": "Velocity of object 1 (m/s)",
@@ -64,14 +65,14 @@ class Chapter13(PhysicsChapter):
                     "r(1)": "Distance from the center of mass M to m (m)",
                     "r(2)": "Distance from the center of mass M to m (m)",
                 },
-                calculation=self.Calculate.conservation_of_energy
+                calculation=self.Calculate.conservation_of_grav_energy
             ),
             Equation(
                 name="Escape velocity",
                 formula="v(esc) = √(2GM/R)",
                 variables={
                     "V(esc)": "Escape velocity (m/s)",
-                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² [constant]",
+                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² (constant)",
                     "M": "Mass of the stellar body (kg)",
                     "R": "Radius of the stellar body (m)"
                 },
@@ -82,7 +83,7 @@ class Chapter13(PhysicsChapter):
                 formula="V(orbit) = √(GM/r)",
                 variables={
                     "V(esc)": "Escape velocity (m/s)",
-                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² [constant]",
+                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² (constant)",
                     "M": "Mass of the stellar body (kg)",
                     "r": "Altitude of the orbiting body (m)"
                 },
@@ -94,7 +95,7 @@ class Chapter13(PhysicsChapter):
                 variables={
                     "T": "Orbital period (s)",
                     "r": "Altitude of orbiting body (m)",
-                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² [constant]",
+                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² (constant)",
                     "M": "Mass of the stellar body (kg)"
                 },
                 calculation=self.Calculate.orbital_period
@@ -104,14 +105,11 @@ class Chapter13(PhysicsChapter):
                 formula="E = K + U = -GmM/2r",
                 variables={
                     "E": "Total energy of the systen (J)",
-                    "K": "Kinetic energy of the system (J)",
-                    "U": "Potential energy of the system (J)",
                     "r": "Distance between the center of mass of object M and m (m)",
-                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² [constant]",
+                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² (constant)",
                     "M": "Mass of the stellar body (kg)",
                     "m": "Mass of the orbiting body (kg)"
-                },
-                calculation=self.Calculate.energy_circular_orbit
+                }
             ),
             Equation(
                 name="Orbital equation",
@@ -130,7 +128,7 @@ class Chapter13(PhysicsChapter):
                 variables={
                     "T": "The orbital period of the stellar body (s)",
                     "a": "Semi-major axis of the orbit",
-                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² [constant]",
+                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² (constant)",
                     "M": "Mass of the stellar body (kg)"
                 },
                 calculation=self.Calculate.keplers_third_law
@@ -140,10 +138,11 @@ class Chapter13(PhysicsChapter):
                 formula="R(S) = 2GM/c²",
                 variables={
                     "R(S)": "Schwarzschild radius (m)",
-                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² [constant]",
+                    "G": "Newton's gravitational constant:  6.674 × 10⁻¹¹ m³ kg⁻¹ s⁻² (constant)",
                     "M": "Mass of the stellar body (kg)",
                     "c": "Speed of light: 2.998 × 10⁸ m/s (constant) "
-                }
+                },
+                calculation=self.Calculate.schwarzschild_radius
             )
         ]
 
@@ -275,6 +274,12 @@ class Chapter13(PhysicsChapter):
                 mass_2 (Optional[float], optional): mass of stellar body 2 [kg]. Defaults to None.
                 distance (Optional[float], optional): distance between the center of mass of both bodies [m]. Defaults to None.
 
+            Raises:
+                ValueError: "We are operating with massive objects.
+                    Make sure all objects have a mass greater than zero
+                ValueError: "Distance cannot be a negative value."
+                ValueError: "Negative radicand yields a complex number. Check your values."
+
             Returns:
                 float: the result of whichever variable was left equal to None
             """
@@ -333,6 +338,11 @@ class Chapter13(PhysicsChapter):
                 mass (Optional[float], optional): mass of the stellar body [kg]. Defaults to None.
                 distance (Optional[float], optional): radius of the stellar body [m]. Defaults to None.
 
+
+            Raises:
+                ValueError: "Distance cannot be a value that is less than or equal to zero."
+                ValueError: "Distance cannot be a negative value."
+
             Returns:
                 float: the result of whichever variable was left equal to None
             """
@@ -358,6 +368,77 @@ class Chapter13(PhysicsChapter):
 
         @staticmethod
         def gravitational_potential(
+            potential_energy: Optional[float]=None,
+            mass_body: Optional[float]=None,
+            mass_object: Optional[float]=None,
+            distance: Optional[float]=None
+        ) -> float:
+            """
+            Function calculates the gravitational potential energy betweem 
+            a stellar and an orbiting object as a function of both masses 
+            and the distance between the center of mass of each object. 
+            Can also calculate for desired variable when arg == None
+            and all other args have values.
+
+            Args:
+                potential_energy (Optional[float], optional): gravitational potential energy [J]. Defaults to None.
+                mass_body (Optional[float], optional): mass of stellar body [kg]. Defaults to None.
+                mass_object (Optional[float], optional): mass of object [kg]. Defaults to None.
+                distance (Optional[float], optional): distance between the two objects [m]. Defaults to None.
+
+            Raises:
+                ValueError: "Distance between the two bodies must be greater than zero."
+                ValueError: "We are operating with massive objects. Make sure all objects have a mass greater than zero."
+                ValueError: "Mass cannot be negative. Check your values."
+                ValueError: "Distance cannot be negative. Check your values."
+
+            Returns:
+                float: _description_
+            """
+
+            if distance is not None and distance <= 0.0:
+                raise ValueError("Distance between the two bodies must be \
+                greater than zero.")
+            
+            if mass_body is not None and mass_body <= 0.0
+            or mass_object is not None and mass_object <= 0.0:
+                raise ValueError("We are operating with massive objects. \
+                Make sure all objects have a mass greater than zero.")
+            
+            if mass_body == None:
+
+                # Calculates the mass of the stellar body
+                result: float = -(potential_energy * distance) / (G * mass_object)
+
+                if result < 0.0:
+                    raise ValueError("Mass cannot be negative. Check your values.")
+                else:
+                    return result
+            
+            if mass_object == None:
+
+                # Calculates the mass of the orbiting object
+                result: float = -(potential_energy * distance) / (G * mass_body)
+
+                if result < 0.0:
+                    raise ValueError("Mass cannot be negative. Check your values.")
+                else:
+                    return result   
+            
+            if distance == None:
+
+                result: float = -(G * mass_body * mass_object) / potential_energy
+
+                if result < 0.0:
+                    raise ValueError("Distance cannot be negative. \
+                        Check your values.")
+                else:
+                    return result  
+
+            return -(G * mass_body * mass_object) / distance 
+
+        @staticmethod
+        def conservation_of_grav_energy(
             mass_object: Optional[float]=None,
             mass_body: Optional[float]=None,
             velocity_1: Optional[float]=None,
@@ -365,7 +446,35 @@ class Chapter13(PhysicsChapter):
             distance_1: Optional[float]=None,
             distance_2: Optional[float]=None
         ) -> float:
+            """
+            Function calculates the conservation of gravitational energy betweem 
+            a stellar and an orbiting object as a function of both masses,
+            the distance and velocitie at point 1 and point 2. 
+            Can also calculate for desired variable when arg == None
+            and all other args have values.
 
+            Args:
+                mass_object (Optional[float], optional): mass of orbiting object [kg]. Defaults to None.
+                mass_body (Optional[float], optional): mass of stellar object [kg]. Defaults to None.
+                velocity_1 (Optional[float], optional): velocity at moment 1 [m/s]. Defaults to None.
+                velocity_2 (Optional[float], optional): velocity at moment 2 [m/s]. Defaults to None.
+                distance_1 (Optional[float], optional): distance of center of mass 
+                                                        from mass_body and mass_stellar at moment 1 [m]. Defaults to None.
+                distance_2 (Optional[float], optional): distance of center of mass 
+                                                        from mass_body and mass_stellar at moment 2 [m]. Defaults to None.
+
+            Raises:
+                ValueError: "Distance cannot be a negative value."
+                ValueError: "We are operating with massive objects.
+                            Make sure all objects have a mass greater than zero."
+                ValueError: "Divison by zero is undefined."
+                ValueError: "Mass of orbiting body cancels out and
+                            cannot be determined."
+                ValueError: "Negative radicand yields a complex number. Check your values."
+                
+                Returns:
+                float: the result of whichever variable was left equal to None
+            """
         if distance_1 is not None and distance_1 <= 0.0
         or distance_2 is not None and distance_2 <= 0.0:
             raise ValueError("Distance cannot be a negative value.")
@@ -446,4 +555,109 @@ class Chapter13(PhysicsChapter):
             
             return sqrt(radicand)
         
+        @staticmethod
+        def escape_velocity(
+            escape_vel: Optional[float]=None,
+            mass_body: Optional[float]=None,
+            radius: Optional[float]=None
+        ) -> float:
+            """
+            Function calculates the escape velocity for a stellar body as a
+            function of its mass and its radius.
+            Can also calculate for desired variable when arg == None
+            and all other args have values.
+
+            Args:
+                escape_vel (Optional[float], optional): escape velocity [m/s]. Defaults to None.
+                mass_body (Optional[float], optional): mass of stellar body [kg]. Defaults to None.
+                radius (Optional[float], optional): radius of stellar body. Defaults to None.
+
+            Raises:
+                ValueError: "We are operating with massive objects. Make sure all objects have a mass greater than zero."
+                ValueError: "Radius of stellar body must be greater than zero."
+                ValueError: "Negative square root yields a complex number. Check your values."
+                ValueError: "Divison by zero is undefined."
+
+            Returns:
+                float: the result of whichever variable was left equal to None
+            """
+
+            if mass_body is not None and mass_body <= 0:
+                raise ValueError(
+                "We are operating with massive objects. \
+                Make sure all objects have a mass greater than zero."
+            )
+
+            if radius is not None and radius <= 0:
+                raise ValueError("Radius of stellar body must be greater than zero.")
+
+            if mass_body == None:
+
+                # Calculates the mass of the stellar body
+                if escape_vel < 0.0:
+                    raise ValueError("Negative square root yields a complex number.\
+                        Check your values.")
+
+                return (sqrt(escape_vel) * radius) / (2.0 * G)
+            
+            if radius == None:
         
+                if escape_vel < 0.0:
+                    raise ValueError("Negative square root yields a complex number.\
+                        Check your values.")
+
+                 if escape_vel == 0.0:
+                    raise ValueError("Divison by zero is undefined.")
+                
+                return (2.0 * G * mass_body) / sqrt(escape_vel)
+            
+            radicand: float = (2.0 * G * mass_body) / radius
+
+            if radicand < 0.0: 
+                raise ValueError("Negative square root yields a complex number.\
+                        Check your values.")
+            
+            return sqrt(radicand)
+
+        @staticmethod
+        def orbital_velocicty(
+            orbital_vel: Optional[float]=None,
+            mass_body: Optional[float]=None,
+            distance: Optional[float]=None,
+        ) -> float:
+            return 0.0
+        
+        @staticmethod
+        def orbital_period(
+            orbital_period: Optional[float]=None,
+            distance: Optional[float]=None,
+            mass_body: Optional[float]=None
+        ) -> float:
+            return 0.0
+        
+        @staticmethod
+        def orbital_equation(
+            semi_major_axis: Optional[float]=None,
+            eccentricity: Optional[float]=None,
+            distance: Optional[float]=None,
+            theta: Optional[float]=None
+        ) -> float:
+            return 0.0
+        
+        @staticmethod
+        def keplers_third_law(
+            orbital_period: Optional[float]=None,
+            semi_major_axis: Optional[float]=None,
+            mass_body: Optional[float]=None
+        ) -> float:
+            return 0.0
+        
+        @staticmethod
+        def schwarzschild_radius(
+            shwarz_radius: Optional[float]=None,
+            mass_body: Optional[float]=None,
+        ) -> float: 
+            return 0.0
+
+                
+
